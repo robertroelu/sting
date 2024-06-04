762851f282014eb950b6808c44c3200ff5a91cc0
Róbert Klimant <roboklimant@gmail.com>
2024-06-04 13:01:37 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import { swiper } from "src/swiper2";
+import { swiper } from 'src/swiper2';
 
-import { annualReports } from "./annualReport";
-import { jobylon } from "./jobylon";
-import { library } from "./library";
-import { openCoaching } from "./openCoaching";
-import { senja } from "./senja";
+import { annualReports } from './annualReport';
+import { jobylon } from './jobylon';
+import { library } from './library';
+import { openCoaching } from './openCoaching';
+import { senja } from './senja';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
+  alert('ide');
   annualReports();
   jobylon();
   library();
